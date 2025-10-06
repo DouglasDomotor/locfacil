2f556e0bd45edd750d92ce1cee3313826ed85438
Douglas Domotor <dmtr82do@gmail.com>
2025-10-06 20:41:18 -0300

    feat: implement core app views and URL routing, add initial tests for index view

---
+++ core/tests/test_urls.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class CoreUrlsTestCase(TestCase):
+    def test_index_status_code(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<h1>Bem vindo à minha primeira página!!!!</h1>')
+        
